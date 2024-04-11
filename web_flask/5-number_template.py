15f3533f69d23c55d8b0b020644def6abb5f15f0
tingru0515 <tingru0515@gmail.com>
2024-04-11 10:17:07 +1000

    q5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -20,7 +20,7 @@ def c(text):
     return "C %s" % text.replace("_", " ")
 
 
-@app.route("/python/", defaults="is cool", strict_slashes=False)
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
     return "Python %s" % text.replace("_", " ")
@@ -35,17 +35,13 @@ def n_number(n):
         return "Not a number", 404
 
 
-# @app.route("/number_template/<n>", strict_slashes=False)
-# def html_page(n):
-#     try:
-#         number = int(n)
-#         return render_template("5-number.html", n=number)
-#     except ValueError:
-#         return "Not a number", 404
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<n>", strict_slashes=False)
 def html_page(n):
-    if type(n) is int:
-        return render_template("5-number.html", n=n)
+    try:
+        number = int(n)
+        return render_template("5-number.html", n=number)
+    except ValueError:
+        return "Not a number", 404
 
 
 app.run(host="0.0.0.0", port=5000)
