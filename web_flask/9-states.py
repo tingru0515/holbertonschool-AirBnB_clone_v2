f7a1a4f8bc6251d5b4412bb80a16a2e9c33f2af4
tingru0515 <tingru0515@gmail.com>
2024-04-12 13:33:29 +1000

    q10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,18 +12,38 @@ app = Flask(__name__)
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id=None):
     all_states = storage.all(State).values()
+    data = []
+
+    for state in all_states:
+        item = {'state': state, 'cities': []}
+        cities = state.cities
+        if cities:
+            sorted_cities = sorted(cities, key=lambda c: c.name)
+            item['cities'] = sorted_cities
+        data.append(item)
 
     if id:
         the_state = storage.get(State, id)
         if the_state:
-            the_state.cities = sorted(the_state.cities,
-                                      key=lambda city: city.name)
             return render_template("9-states.html", the_state=the_state)
         else:
             return render_template("9-states.html", not_found=True)
     else:
-        states = sorted(all_states, key=lambda state: state.name)
-        return render_template("9-states.html", states=states)
+        sorted_states = sorted(all_states, key=lambda s: s.name)
+        return render_template("9-states.html", states=sorted_states)
+    # all_states = storage.all(State).values()
+
+    # if id:
+    #     the_state = storage.get(State, id)
+    #     if the_state:
+    #         the_state.cities = sorted(the_state.cities,
+    #                                   key=lambda city: city.name)
+    #         return render_template("9-states.html", the_state=the_state)
+    #     else:
+    #         return render_template("9-states.html", not_found=True)
+    # else:
+    #     states = sorted(all_states, key=lambda state: state.name)
+    #     return render_template("9-states.html", states=states)
 
 
 @app.teardown_appcontext
